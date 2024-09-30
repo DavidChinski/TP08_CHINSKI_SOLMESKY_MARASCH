5178a3f367870b8e33ddcc78efc7edf11a064a4f
DAVID CHINSKI <48592365@ort>
2024-09-30 09:39:42 -0300

    hacer 4 ej mas

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -10,3 +10,31 @@ function ejercicio2() {
     const resultado = separarPorGuiones(lista);
     document.getElementById("resultado").innerHTML = resultado;
 }
+
+function ejercicio3() {
+    const cadena = prompt("Ingresa una cadena de texto:");
+    const numero = parseInt(prompt("Ingresa el número de caracteres a mostrar:"));
+    const resultado = recortarCadena(cadena, numero);
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function ejercicio4() {
+    const cadena = prompt("Ingresa una cadena de texto:");
+    const palabraOriginal = prompt("Ingresa la palabra que quieres reemplazar:");
+    const palabraNueva = prompt("Ingresa la nueva palabra:");
+    const resultado = reemplazarPalabras(cadena, palabraOriginal, palabraNueva);
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function ejercicio5() {
+    const nombres = prompt("Ingresa una lista de nombres separados por comas:");
+    const resultado = filtrarNombresConA(nombres);
+    document.getElementById("resultado").innerHTML = `Nombres que empiezan con 'A': ${resultado.join(', ')}`;
+}
+
+function ejercicio6() {
+    const numeros = prompt("Ingresa una lista de números separados por comas:");
+    const arrayNumeros = numeros.split(',')
+    const resultado = duplicarNumeros(arrayNumeros);
+    document.getElementById("resultado").innerHTML = `Array duplicado: ${resultado.join(', ')}`;
+}
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,27 @@
 function calcularRecaudacionTotal(pedidos) {
     const pedidosArray = pedidos.split(',').map(pedido => {
         const [nombre, total] = pedido.split(':');
-        return parseFloat(total);
+        return parseFloat(total)    ;
     });
     return pedidosArray.reduce((acum, actual) => acum + actual, 0);
 }
 
 function separarPorGuiones(lista) {
     return lista.split(',').join(' - ');
+}
+
+function recortarCadena(cadena, numero) {
+    return cadena.slice(0, numero);
+}
+
+function reemplazarPalabras(cadena, palabraOriginal, palabraNueva) {
+    return cadena.replaceAll(palabraOriginal, palabraNueva);
+}
+
+function filtrarNombresConA(nombres) {
+    return nombres.split(',').filter(nombre => nombre.startsWith('A'));
+}
+
+function duplicarNumeros(array) {
+    return array.map(numero => numero * 2);
 }
